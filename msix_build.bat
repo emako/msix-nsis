1c73d6a94d35f2ecb366b7a3c67e5828a3d2f874
ema <mccoy39082@163.com>
2022-09-04 06:34:35 +0800

    Added rename

--- msix_build.bat
+++ msix_build.bat
@@ -1,7 +1,5 @@
-@REM msix
-@REM copy msix\App.Packaging\AppPackages\App.Packaging_1.0.0.0_x64_Test\App.Packaging_1.0.0.0_x64.cer App.cer
-@REM copy msix\App.Packaging\AppPackages\App.Packaging_1.0.0.0_x64_Test\App.Packaging_1.0.0.0_x64.msix App.msix
-@REM msixbundle
 copy msix\App.Packaging\AppPackages\App.Packaging_1.0.0.0_Test\App.Packaging_1.0.0.0_x64.cer App.cer
 copy msix\App.Packaging\AppPackages\App.Packaging_1.0.0.0_Test\App.Packaging_1.0.0.0_x64.msixbundle App.msixbundle
+rename App.Packaging__*_x64.cer App.cer
+rename App.Packaging__*_x64.msixbundle App.msixbundle
 @pause
